65afcbd29b7d694f4a38d604fc68676227a26c10
Yeraldiner <yeraldiner@uninorte.edu.co>
2021-12-19 00:50:37 -0500

    backperfil

--- src/controllers/perfilController.js
+++ src/controllers/perfilController.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
-const rutasUsuario = Router();
+const rutasPerfil = Router();
 const { usuarioModel } = require("../models/usuarioModel.js")
 const { verify } = require("jsonwebtoken");
 
-rutasUsuario.post("/mostrarperfil", async function (req, res) {
+rutasPerfil.post("/mostrarperfil", async function (req, res) {
     const authorization = req.headers.authorization;
     try {
         const token = authorization.split(" ")[1];
@@ -16,7 +16,7 @@ rutasUsuario.post("/mostrarperfil", async function (req, res) {
     
 });
 
-rutasUsuario.post("/modificarperfil", async function (req, res) {
+rutasPerfil.post("/modificarperfil", async function (req, res) {
     const authorization = req.headers.authorization;
     const usuario = req.body;
     try {
@@ -29,4 +29,4 @@ rutasUsuario.post("/modificarperfil", async function (req, res) {
     }
 });
 
-exports.rutasUsuario = rutasUsuario;
+exports.rutasPerfil = rutasPerfil;
